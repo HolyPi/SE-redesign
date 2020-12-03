5e2d62af8ef0b84e5627edd21a8ba90e073ca552
HolyPi <joanelly2018@gmail.com>
2020-12-02 21:46:42 -0500

    [Add] Navbar 1 & 2 Styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import Header from './Header/Header';
+import Store from './Store/Store';
+import Sqhl from './Sq-hl/Sq-hl';
+import Footer from './Footer/Footer';
+import Navbar2 from './Navbar2/Navbar2';
+import Navbar from './Navbar/Navbar';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
+    <Router>
     <div className="App">
+      <Navbar2 />
+      <Navbar />
+
     </div>
+    </Router>
   );
 }
 
