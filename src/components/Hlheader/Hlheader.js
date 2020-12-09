0da99ae9c7b206e68fb5a9e90c4d994d66989ae7
HolyPi <joanelly2018@gmail.com>
2020-12-08 21:29:05 -0500

    [Add] Parallax

--- src/components/Hlheader/Hlheader.js
+++ src/components/Hlheader/Hlheader.js
@@ -1,16 +1,27 @@
 import React from 'react'
 import './Hlheader.css'
-import log from '../../static/images/logo.png'
+import { useEffect, useState } from "react"
+import log from '../../static/images/3.png'
 import gl from '../../static/images/gloria2.png'
 
 export default function Hlheader() {
+        const [offsetY, setOffset] = useState(0)
+        useEffect(() => {
+          function handleScroll() {
+            setOffset(window.pageYOffset)
+          }
+          window.addEventListener("scroll", handleScroll)
+          return () => {
+            window.removeEventListener("scroll", handleScroll)
+          }
+        }, [])
+      
     return (
         <div className="hl-container">
-            <h3>Highlights</h3>
-            <div className="hlheader">
-            <img src={log} class="logo"></img>
-            <img src={gl} class="hlg"></img>
-           
+            <h3>Store Highlights</h3>
+            <div className="hlheader" style={{ transform: `translateY(-${offsetY * 0.5}px)` }}>
+            <img src={log} class="logo" style={{ transform: `translateY(-${offsetY * 0.5}px)` }}></img>
+            <img src={gl} class="hlg" ></img>
             </div>
         </div>
 
--- src/components/Navbar2/Navbar2.js
+++ src/components/Navbar2/Navbar2.js
@@ -7,8 +7,8 @@ export default function Navbar2() {
     return (
         <header>
         <div className="nav">
+        <img src={slogo} alt="logo"></img>
             <ul className="nav-links2">
-            <img src={slogo} alt="logo"></img>
                 <li><Link to="/">Log in / Sign Up</Link></li>
                 <li><Link to="/">Wish List</Link></li>
                 <li><Link to="/">Help</Link></li>
